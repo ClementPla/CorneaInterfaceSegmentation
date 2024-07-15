117be191f96ba327c3d3b98a2c3c9fd172f5394c
ClementPla <clement.playout@polymtl.ca>
2024-07-15 11:55:14 -0400

    reorganize code

--- cornea/generate/corrupt.py
+++
@@ -1,9 +0,0 @@
-from cornea.models.interface import get_interface_line
-
-
-def create_hole(image, mask):
-    interface_line = get_interface_line(mask)
-
-    hole = image.clone()
-
-    return hole
